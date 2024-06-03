ec24f72cb473beb534197a8f5332b1cb6b55bceb
Medet <mtoksanbai@mail.ru>
2024-06-03 12:39:46 +0200

    f change

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
@@ -10,9 +11,15 @@ import (
 	_ "github.com/lib/pq"
 )
 
+type User struct {
+	Id   int
+	Name string
+	Age  int
+}
+
 var database *sql.DB
 
-func homeHandler(c echo.Context) error {
+func home(c echo.Context) error {
 	if c.Request().Method == http.MethodPost {
 		id := c.FormValue("id")
 		name := c.FormValue("name")
@@ -27,6 +34,26 @@ func homeHandler(c echo.Context) error {
 	return c.File("hello.html")
 }
 
+func secPage(c echo.Context) error {
+	var u User
+	rows, err := database.Query("SELECT * FROM public.fortestdb")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	user := []User{}
+
+	for rows.Next() {
+		err = rows.Scan(&u.Id, &u.Name, &u.Age)
+		if err != nil {
+			panic(err)
+		}
+		user = append(user, u)
+	}
+	tmpl, _ := template.ParseFiles("second.html")
+	return tmpl.Execute(c.Response().Writer, user)
+}
+
 func main() {
 	conn := "user=postgres password=meda13 dbname=fortestdb sslmode=disable"
 	db, err := sql.Open("postgres", conn)
@@ -37,8 +64,9 @@ func main() {
 	defer db.Close()
 
 	e := echo.New()
-	e.GET("/", homeHandler)
-	e.POST("/", homeHandler)
+	e.GET("/", home)
+	e.POST("/", home)
+	e.GET("/users", secPage)
 
 	fmt.Println("loading...")
 	e.Start(":8181")
